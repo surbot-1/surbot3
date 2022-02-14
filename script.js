3fbb4e49d7a7cac01d41843cb2b31e953105c089
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-02-15 00:45:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 let d = new Date();
-document.getElementByID("root2").innerHTML += "<h1>Today's date is " + d + "</h1>"
+document.getElementByID("root2").innerHTML += '<div>' + " Today's date is " + d ;
