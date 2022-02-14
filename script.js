fa203d80f517f81bd57fed495dd0586483877a20
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-02-15 01:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 alert('You triggered an alert!');
 let d = new Date();
-document.getElementById("root2").innerHTML += '<br>' + "Today's date is";
+document.body.innerHTML += '<br>' + "Today's date is";
