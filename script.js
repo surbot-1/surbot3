7ec12725a6461777c33b587ed002a951383c4564
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-02-15 03:13:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-alert('Alert 3!');
+alert('Alert 4!');
 let d = new Date();
-document.getElementById("root2").innerHTML += '<br>' + "Today's date is"+ d;
+document.getElementById("root").innerHTML += '<br>' + "Today's date is"+ d;
